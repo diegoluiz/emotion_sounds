ef7fece6afbfacc77b1ec72b72b70aa4bea56d14
Diego Luiz <diego@diegoluiz.com>
2016-09-13 18:53:41 +0100

    added the file

---
+++ sounds.sh
@@ -0,0 +1,7 @@
+alias xfiles="curl -s https://www.myinstants.com/media/sounds/expedientes-secretos-x-musica22.mp3 | mpg123 -"
+alias sad="curl -s https://www.myinstants.com/media/sounds/tf_nemesis.mp3 | mpg123 -"
+alias english="curl -s https://www.myinstants.com/media/sounds/pulp-fiction-english-motherfucker-do-you-speak-it.mp3 | mpg123 -"
+alias grilo="curl -s https://www.myinstants.com/media/sounds/grilo_1.mp3 | mpg123 -"
+alias starwars="curl -s https://www.myinstants.com/media/sounds/imperial_march.mp3 | mpg123 -"
+alias gogo="curl -s https://www.myinstants.com/media/sounds/8e8118_counter_strike_go_go_go_sound_effect.mp3 | mpg123 -"
+alias noo="curl -s https://www.myinstants.com/media/sounds/nooo.mp3 | mpg123 -"
